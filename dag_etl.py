c5c38f86ec85444081102ca5cddf6251747077df
Inés Torres <mariainestorres89@gmail.com>
2024-09-24 05:11:22 -0300

    Update dag_etl.py

--- dag_etl.py
+++ dag_etl.py
@@ -1,26 +1,34 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 from etl import (
     get_data_api,
     conexion_db,
-    carga_datos_db)
+    envio_de_email
+    )
 
 
 # Defino los argumentos por defecto del DAG
 default_args = {
     'owner': 'airflow',
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2024, 9, 1),
+    'retry_delay': timedelta(minutes=1),
+    "email":'mariainestorres89@gmail.com',
+    "email_on_failure":True,
+    "email_on_success":True,
+    "email_on_retry":True,
 }
 
 # Defino el DAG
-with DAG('consulta_api_dag',
+with DAG(
+         dag_id='consulta_api_dag',
+         start_date=datetime(2024, 9, 1),
+         schedule='@daily',
+         catchup=False,
          default_args=default_args,
-         schedule_interval='@daily',
-         catchup=False) as dag:
+) as dag:
     
     # Tarea para conectarse a la API y extraer datos
         get_data_api_task = PythonOperator(
@@ -28,24 +36,18 @@ with DAG('consulta_api_dag',
             python_callable=get_data_api,
             provide_context=True,
         )
-    # Tarea para transformar los datos
-#        transformacion_task = PythonOperator(
-#            task_id='transformacion',
-#            python_callable=transformacion,
-#            provide_context=True,
-#        )
            
-    # Tarea para conectarse a la Base de Datos
+    # Tarea para conectarse a la Base de Datos y cargarlos
         conexion_db_task = PythonOperator(
             task_id='conexion_db',
             python_callable=conexion_db,
         ) 
+   
+    # Tarea para envío de mails
+        envio_de_email_task = PythonOperator(
+            task_id='envio_de_email',
+            python_callable=envio_de_email
+        )
         
-    # Tarea para la carga de datos
-        carga_datos_db_task = PythonOperator(
-            task_id='carga_datos_db',
-            python_callable=carga_datos_db,
-        )            
-
 # Defino las dependencias entre tareas
-get_data_api_task >> conexion_db_task >> carga_datos_db_task        
+get_data_api_task >> conexion_db_task >> envio_de_email_task       
